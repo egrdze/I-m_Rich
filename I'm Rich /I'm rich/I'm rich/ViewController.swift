8f307da16213272947c696ea3cda4029ebb31388
Egor <62994823+egrdze@users.noreply.github.com>
2022-08-02 03:31:02 +0300

    Add files via upload

---
+++ I'm Rich /I'm rich/I'm rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm rich
+//
+//  Created by Егор on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+
+
+}
+
---
+++ I'm Rich /I'm rich/I'm richUITests/I_m_richUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_m_richUITestsLaunchTests.swift
+//  I'm richUITests
+//
+//  Created by Егор on 02.08.2022.
+//
+
+import XCTest
+
+class I_m_richUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
